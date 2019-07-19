feaf70628cf63489e1d6316cd653183d36d397fa
Pavel Zagainov <pasha_zigzag@mail.ru>
2019-07-19 13:45:31 +1000

    lesson 2

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
-	$users = [
-		['id' 		=> 1,
-		'username' 	=> 'John Doe',
-		'email' 	=> 'john@example.com'],
-		['id' 		=> 2,
-		'username' 	=> 'Joseph Doe',
-		'email' 	=> 'joseph@example.com'],
-		['id' 		=> 3,
-		'username' 	=> 'Jane Doe',
-		'email' 	=> 'jane123@example.com'],
-		['id' 		=> 4,
-		'username' 	=> 'Rakhim Muratov',
-		'email' 	=> 'rakhim@example.com'],
-		['id' 		=> 13,
-		'username' 	=> 'Pavel Zagainov',
-		'email' 	=> 'pavel-zagainov@yandex.ru']
-	];
+	// $users = [
+	// 	['id' 		=> 1,
+	// 	'username' 	=> 'John Doe',
+	// 	'email' 	=> 'john@example.com'],
+	// 	['id' 		=> 2,
+	// 	'username' 	=> 'Joseph Doe',
+	// 	'email' 	=> 'joseph@example.com'],
+	// 	['id' 		=> 3,
+	// 	'username' 	=> 'Jane Doe',
+	// 	'email' 	=> 'jane123@example.com'],
+	// 	['id' 		=> 4,
+	// 	'username' 	=> 'Rakhim Muratov',
+	// 	'email' 	=> 'rakhim@example.com'],
+	// 	['id' 		=> 13,
+	// 	'username' 	=> 'Pavel Zagainov',
+	// 	'email' 	=> 'pavel-zagainov@yandex.ru']
+	// ];
+
+	$pdo = new PDO('mysql:host=localhost;dbname=marlin-first;charset=utf8;', 'root', '');
+	$sql = 'SELECT * FROM users';
+	$statement = $pdo->query($sql);
+	$users = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
---
+++ marlin-first.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июл 19 2019 г., 06:37
+-- Версия сервера: 5.6.41
+-- Версия PHP: 7.2.10
+
+CREATE DATABASE `marlin-first`;
+USE `marlin-first`;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `marlin-first`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`) VALUES
+(1, 'Rakhim Muratov', 'rakhim@example.com'),
+(2, 'Pavel Zagainov', 'pavel-zagainov@yandex.ru');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
